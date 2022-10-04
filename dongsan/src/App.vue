130eb33ba7240dbbb57a005f19c57266ebdca6a7
shinjoo95 <shinjoo95@naver.com>
2022-10-04 18:02:11 +0900

    반복문 v-for

--- dongsan/src/App.vue
+++ dongsan/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    직방
-    <h4 :style="font">{{products[0]}}</h4>  <!-- html 속성도 데이터바인딩 가능 :속성="데이터이름"-->
-    <p> {{ price1 }} 만원</p>
+     <!-- 반복문으로 메뉴 만들기 
+      <태그 v-for = " 작명 in 몇회?(데이터도 넣을 수 있음) :key="작명">
+      :key=""용도 - 반복문 쓸 때 꼭 써야됨, 반복문 돌린 요소를 컴퓨터가 구분하기 위해 씀 
+      array/object 자료형 집어넣기 가능, 자료안의 데이터 갯수만큼 반복됨 
+      작명한 변수는 데이터안의 자료가 됨 -->
+     <div class="menu">
+      <a v-for="a in 메뉴" :key="a">{{a}}</a> <!-- 변수 작명은 2개까지 가능,(a,i) 왼쪽변수는 array내의 데이터, 오른쪽변수는 1씩 증가하는 정수 -->
+    </div>
+  <div v-for="(a,i) in products" :key="i">
+    <h4 >{{products[i]}}</h4>  <!-- html 속성도 데이터바인딩 가능 :속성="데이터이름"-->
+    <p> 60 만원</p>
   </div>
-  <div>
+  <!-- <div>
     <h4>{{products[1]}}</h4>
-    <p>{{ price2 }}만원</p>
+    <p>50 만원</p>
   </div>
   <div>
     <h4>{{products[2]}}</h4>
-    <p>{{ price3 }}만원</p>
-  </div>
+    <p>80 만원</p>
+  </div> -->
+
+
 </template>
 
 <script>
@@ -28,10 +36,8 @@ export default {
   name: 'App',
   data(){
     return{
+      메뉴 : ['home', 'shop', 'about'],
       //데이터는 object 자료로 저장
-      price1 : 80,
-      price2 : 70, 
-      price3 : 60,
       font : 'color:blue',
       products : ['역삼동원룸', '천호동원룸','마포구원룸']
     }
@@ -49,6 +55,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu{
+  background:  darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding : 10px;
+  font-size: 20px;
 }
 </style>
