5a0632adfbd0deedcf68c4988d8cca272965ba91
shinjoo95 <shinjoo95@naver.com>
2022-10-09 23:16:06 +0900

    nav

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -6,7 +6,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Joobong</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,10 +19,10 @@
           <a class="nav-link" href="#">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Shop</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link disabled">Contact</a>
         </li>
       </ul>
     </div>
@@ -65,4 +65,5 @@ export default {
   color: #2c3e50;
 
 }
+
 </style>
