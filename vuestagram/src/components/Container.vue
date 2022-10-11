8bea42de009b825494d46c54c705fb17dd05a923
shinjoo95 <shinjoo95@naver.com>
2022-10-11 16:42:18 +0900

    setting

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+  components : {
+    Post,
+  },
+  props : {
+    게시글 : Array,
+  }
+}
+</script>
+
+<style>
+  
+</style>
