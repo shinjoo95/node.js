010b2986dfad14736fb2becb891be37d780c8b8e
shinjoo95 <shinjoo95@naver.com>
2022-10-12 20:12:26 +0900

    mitt 라이브러리 셋팅

--- vuestagram/src/main.js
+++ vuestagram/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import mitt from 'mitt'
+let emitter = mitt();           //npm install mitt
+let app = createApp(App)
+app.config.globalProperties.emitter = emitter; //변수보관함 
 createApp(App).mount('#app')
